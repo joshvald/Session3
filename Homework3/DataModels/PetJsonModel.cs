06f3617aedb39f8e23ff14882682c6e5320958c3
joshvald <joshua.valdez@softvision.com>
2023-03-15 21:33:32 +0800

    Add Homework 3 solution and source code - Create a test using GET method - Implementation of Facade and AAA Pattern - Object Assertion

---
+++ Homework3/DataModels/PetJsonModel.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+
+namespace Homework3.DataModels
+{
+    /// <summary>
+    /// Pet JSON Model
+    /// </summary>
+    public class PetJsonModel
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("category")]
+        public Category Category { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("photoUrls")]
+        public string[] PhotoUrls { get; set; }
+
+        [JsonProperty("tags")]
+        public Category[] Tags { get; set; }
+
+        [JsonProperty("status")]
+        public string Status { get; set; }
+    }
+    public partial class Category
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+}
---
+++ Homework3/Helpers/PetHelper.cs
@@ -0,0 +1,29 @@
+﻿using Homework3.DataModels;
+using Homework3.Resources;
+using Homework3.Test.TestData;
+using RestSharp;
+
+namespace Homework3.Helpers
+{
+    /// <summary>
+    /// Class containing all method for pets
+    /// </summary>
+    public class PetHelper
+    {
+        /// <summary>
+        /// Send POST request to add new pet
+        /// </summary>
+        public static async Task<PetJsonModel> AddNewPet(RestClient client)
+        {
+            var newPetData = GeneratePet.PetDetails();
+            var postRequest = new RestRequest(Endpoints.PostPet());
+
+            // Send Post Request to add new pet
+            postRequest.AddJsonBody(newPetData);
+            var postResponse = await client.ExecutePostAsync<PetJsonModel>(postRequest);
+
+            var createdPetData = newPetData;
+            return createdPetData;
+        }
+    }
+}
